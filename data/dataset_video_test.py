94d3fd5226c4be52ef2b254c5fdf2e87f1e3c864
Jingyun Liang <jingyunliang12@gmail.com>
2022-01-31 17:38:29 +0100

    Update dataset_video_test.py

--- data/dataset_video_test.py
+++ data/dataset_video_test.py
@@ -291,3 +291,99 @@ class VideoTestVimeo90KDataset(data.Dataset):
 
     def __len__(self):
         return len(self.data_info['gt_path'])
+    
+    
+class SingleVideoRecurrentTestDataset(data.Dataset):
+    """Single Video test dataset (only input LQ path).
+
+    Supported datasets: Vid4, REDS4, REDSofficial.
+    More generally, it supports testing dataset with following structures:
+
+    dataroot
+    ├── subfolder1
+        ├── frame000
+        ├── frame001
+        ├── ...
+    ├── subfolder1
+        ├── frame000
+        ├── frame001
+        ├── ...
+    ├── ...
+
+    For testing datasets, there is no need to prepare LMDB files.
+
+    Args:
+        opt (dict): Config for train dataset. It contains the following keys:
+            dataroot_gt (str): Data root path for gt.
+            dataroot_lq (str): Data root path for lq.
+            io_backend (dict): IO backend type and other kwarg.
+            cache_data (bool): Whether to cache testing datasets.
+            name (str): Dataset name.
+            meta_info_file (str): The path to the file storing the list of test
+                folders. If not provided, all the folders in the dataroot will
+                be used.
+            num_frame (int): Window size for input frames.
+            padding (str): Padding mode.
+    """
+
+    def __init__(self, opt):
+        super(SingleVideoRecurrentTestDataset, self).__init__()
+        self.opt = opt
+        self.cache_data = opt['cache_data']
+        self.lq_root = opt['dataroot_lq']
+        self.data_info = {'lq_path': [], 'folder': [], 'idx': [], 'border': []}
+        # file client (io backend)
+        self.file_client = None
+
+        self.imgs_lq = {}
+        if 'meta_info_file' in opt:
+            with open(opt['meta_info_file'], 'r') as fin:
+                subfolders = [line.split(' ')[0] for line in fin]
+                subfolders_lq = [osp.join(self.lq_root, key) for key in subfolders]
+        else:
+            subfolders_lq = sorted(glob.glob(osp.join(self.lq_root, '*')))
+
+        for subfolder_lq in subfolders_lq:
+            # get frame list for lq and gt
+            subfolder_name = osp.basename(subfolder_lq)
+            img_paths_lq = sorted(list(utils_video.scandir(subfolder_lq, full_path=True)))
+
+            max_idx = len(img_paths_lq)
+
+            self.data_info['lq_path'].extend(img_paths_lq)
+            self.data_info['folder'].extend([subfolder_name] * max_idx)
+            for i in range(max_idx):
+                self.data_info['idx'].append(f'{i}/{max_idx}')
+            border_l = [0] * max_idx
+            for i in range(self.opt['num_frame'] // 2):
+                border_l[i] = 1
+                border_l[max_idx - i - 1] = 1
+            self.data_info['border'].extend(border_l)
+
+            # cache data or save the frame list
+            if self.cache_data:
+                logger.info(f'Cache {subfolder_name} for VideoTestDataset...')
+                self.imgs_lq[subfolder_name] = utils_video.read_img_seq(img_paths_lq)
+            else:
+                self.imgs_lq[subfolder_name] = img_paths_lq
+
+        # Find unique folder strings
+        self.folders = sorted(list(set(self.data_info['folder'])))
+
+    def __getitem__(self, index):
+        folder = self.folders[index]
+
+        if self.cache_data:
+            imgs_lq = self.imgs_lq[folder]
+        else:
+            imgs_lq = utils_video.read_img_seq(self.imgs_lq[folder])
+
+        return {
+            'L': imgs_lq,
+            'folder': folder,
+            'lq_path': self.imgs_lq[folder],
+        }
+
+    def __len__(self):
+        return len(self.folders)
+
